442958422221072f56864537e7fd6cbc59d35be6
Stefano Sena <91676539+TeaZea@users.noreply.github.com>
2023-12-14 21:17:00 +0000

    Added a quick description to the projects model and migrated. Updated the text details on super admin.

--- portfolio_v2/projects/models.py
+++ portfolio_v2/projects/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
+    quick_description = models.TextField(default="quick description")
     technology = models.CharField(max_length=20)
 
     def __str__(self):
